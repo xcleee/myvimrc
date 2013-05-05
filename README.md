956efb91b4282511e8cf66e3d92ddeb7bfbf04e7
xclee <xiaochaoi2010@gmail.com>
2013-05-05 14:22:16 +0800

    back up

---
+++ README.md
@@ -0,0 +1,1 @@
+personal backup, little changed from Maple's vim config
---
+++ template/cppmain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
